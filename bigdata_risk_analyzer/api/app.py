af44d4432ebb87acc7f5cf8dcecf4cc18bd74c70
Alberto Martinez <amartinezg@ravenpack.com>
2025-10-08 09:20:07 +0200

    examples moved to specific file examples.py. example uuid is sent to the FE as a parameter. created function to retrieve any UUID data in case we want to expand the functionality for retrieving previous executions

--- bigdata_risk_analyzer/api/app.py
+++ bigdata_risk_analyzer/api/app.py
@@ -10,6 +10,7 @@ from fastapi.staticfiles import StaticFiles
 from sqlmodel import Session, SQLModel, create_engine
 
 from bigdata_risk_analyzer import LOG_LEVEL, __version__, logger
+from bigdata_risk_analyzer.api.examples import EXAMPLE_UUID
 from bigdata_risk_analyzer.api.models import (
     ExampleWatchlists,
     RiskAnalysisRequest,
@@ -95,6 +96,7 @@ async def sample_frontend(_: str = Security(query_scheme)) -> HTMLResponse:
     # Get example values from the schema for all fields
     example_values = get_example_values_from_schema(RiskAnalysisRequest)
     example_values["example_watchlists"] = list(dict(ExampleWatchlists).values())
+    example_values["example_request_id"] = str(EXAMPLE_UUID)
 
     return HTMLResponse(
         content=loader.get_template("api/index.html.jinja").render(**example_values),
