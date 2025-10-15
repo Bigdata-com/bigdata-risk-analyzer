a0533194355bf29594a86526a5bef117cd37c92e
Paco Gomez <fgomez@ravenpack.com>
2025-10-15 17:34:53 +0200

    First release of new front end

--- bigdata_risk_analyzer/__main__.py
+++ bigdata_risk_analyzer/__main__.py
@@ -3,4 +3,4 @@ from bigdata_risk_analyzer.api.app import app
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info")
+    uvicorn.run(app, host="0.0.0.0", port=8001, log_level="info")
