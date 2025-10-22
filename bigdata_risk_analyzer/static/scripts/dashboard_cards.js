8ab5232a70a08d5b2325deb7692634b9a645190e
Paco Gomez <fgomez@ravenpack.com>
2025-10-23 00:39:37 +0200

    Same style for export buttons

--- bigdata_risk_analyzer/static/scripts/dashboard_cards.js
+++ bigdata_risk_analyzer/static/scripts/dashboard_cards.js
@@ -31,7 +31,7 @@ function renderDashboardHelp() {
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
                           d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                 </svg>
-                How to Read This Dashboard
+                Info
             </button>
         </div>
     `;
@@ -114,7 +114,7 @@ function renderDashboardCards(data) {
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
                           d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                 </svg>
-                How to Read This Dashboard
+                Info
             </button>
         </div>
         <div class="grid grid-cols-1 md:grid-cols-3 gap-6 mb-8">
